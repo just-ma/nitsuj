533b590ae5352ae9253aee1a8a2f5d4b40d9a299
justinma98 <justinma3@gmail.com>
2019-12-18 16:21:46 -0800

    create react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import LandingPage from "./pages/landing";
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+      </Switch>
+    </Router>
+  );
+}
---
+++ src/components/cart/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Cart({ stripeToken }) {
+  return <div>Cart</div>;
+}
