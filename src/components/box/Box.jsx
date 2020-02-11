203c87a88d2b2018cbf98c22df6580f02ee92c69
justinma98 <justinma3@gmail.com>
2020-02-10 21:54:15 -0800

    main logo

--- src/components/box/Box.jsx
+++ src/components/box/Box.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Box.scss";
+import nsjLogo from "../../public/nsj.png";
 
 export default function Box() {
-  return <div className="box" />;
+  const [scroll, setScroll] = useState(0);
+  useEffect(() => {
+    document.addEventListener("scroll", () => setScroll(window.scrollY));
+  }, []);
+
+  return (
+    <div className="box">
+      <div className="box__main" style={{ bottom: scroll }}>
+        <img className="box__content" style={{ top: scroll }} src={nsjLogo}/>
+      </div>
+    </div>
+  );
 }
